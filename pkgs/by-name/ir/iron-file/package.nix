0704b4d66d328ded2f6cf2b14638f53043c502c6
Benedikt Weyer <bw.development@pm.me>
2026-07-20 20:02:57 +0200

    init: Add iron-file GTK and Iced packages

---
+++ pkgs/by-name/ir/iron-file/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  fetchFromGitHub,
+  libGL,
+  libxkbcommon,
+  makeWrapper,
+  rustPlatform,
+  vulkan-loader,
+  wayland,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "iron-file";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "benedikt-weyer";
+    repo = "iron-file";
+    rev = "ac26d99f2761043378f23ed77b829761e7261664";
+    hash = "sha256-TGFFBxlzbEuudH6+tiPbI48Un9sTUVjLsHbKPw1ZO3s=";
+  };
+
+  cargoHash = "sha256-WnViXYDX2xq3qlXS+c0OcwWgOWWrc65MIhUHLafDTaE=";
+
+  cargoBuildFlags = [
+    "--package"
+    "iron-file-iced"
+    "--package"
+    "iron-file-backend"
+  ];
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    install -Dm755 "$out/bin/iron-file-backend" \
+      "$out/libexec/iron-file/iron-file-backend"
+    rm "$out/bin/iron-file-backend"
+  '';
+
+  preFixup = ''
+    wrapProgram "$out/bin/iron-file-iced" \
+      --set IRON_FILE_BACKEND_MODE prod \
+      --set IRON_FILE_BACKEND_BIN "$out/libexec/iron-file/iron-file-backend" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
+        libGL
+        libxkbcommon
+        vulkan-loader
+        wayland
+      ]}"
+  '';
+
+  doCheck = false;
+
+  meta = {
+    description = "File browser built with Iced";
+    homepage = "https://github.com/benedikt-weyer/iron-file";
+    license = lib.licenses.mit;
+    mainProgram = "iron-file-iced";
+    platforms = lib.platforms.linux;
+  };
+})
